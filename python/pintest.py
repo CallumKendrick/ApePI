302efb563513b8963564a1f1f04d284f51dc2215
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2016-10-22 23:48:43 +0300

    Revert "Switch pin test"
    This reverts commit 0e921a192dbd7ec6805a051ed5333c7650b68534.

--- python/pintest.py
+++ python/pintest.py
@@ -11,8 +11,8 @@ bus = smbus.SMBus(1)
 address = 0x20
 
 # Set both banks to outputs
-bus.write_byte_data(address, 0x00, 0x00)
-bus.write_byte_data(address, 0x01, 0x00)
+bus.write_byte_data(address,0x00,0x00)
+bus.write_byte_data(address,0x01,0x00)
 
 #Bank mapping
 bankB = 0
@@ -21,11 +21,11 @@ bankA = 1
 def set_led(led, bank):
     data = 2 ** led
     if bank == 1:
-        bus.write_byte_data(address, 0x12, data)
+        bus.write_byte_data(address,0x12,data)
     else:
-        bus.write_byte_data(address, 0x13, data)
+        bus.write_byte_data(address,0x13,data)
 
 def main():
-   set_led(0, bankA)
+   set_led(3, bankA)
 
 if __name__ == "__main__": main()

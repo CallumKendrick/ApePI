67651507603dd51782983a2980f76b727aa2ecb1
CallumKendrick <callumkendrick@hotmail.co.uk>
2016-10-23 00:01:40 +0100

    hopefully fix the app not starting. possibly create more problems

--- node/index.js
+++ node/index.js
@@ -1,20 +1,27 @@
 var express = require("express");
 var app = express();
 var Pusher = require("pusher");
+var bodyParser = require('body-parser');
 
 var pusherConfig = require("./pusherConfig.json");
 var pusher = new Pusher(pusherConfig);
 
-app.use("/", express.static("html"));
+app.use("/", express.static(__dirname + "html"));
+
+app.use(bodyParser.urlencoded( {
+  extended: true
+}));
 
 app.listen(3000 , function() {
     console.log("running");
 });
 
-app.get("/test", function(request, response) {
+app.post("/test", function(request, response) {
+    console.log(request.body);
+
     pusher.trigger("questions", "new-question", {
-        text: "hello world",
-        username: "server"
+        text: request.body.userQuestion,
+        username: request.body.username
     }, function(error, request, response) {
         console.log(error);
     });

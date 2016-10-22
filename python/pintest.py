ae02568d63aceedcb0863e9e86dfdefcb6c36b64
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2016-10-22 23:41:39 +0300

    Make led numbering make sense

--- python/pintest.py
+++ python/pintest.py
@@ -14,20 +14,18 @@ address = 0x20
 bus.write_byte_data(address,0x00,0x00)
 bus.write_byte_data(address,0x01,0x00)
 
-#LED pin mapping
-led1 = 1
-
 #Bank mapping
 bankB = 0
 bankA = 1
 
-def set_led(data,bank):
+def set_led(led, bank):
+    data = 2 ** led
     if bank == 1:
         bus.write_byte_data(address,0x12,data)
     else:
         bus.write_byte_data(address,0x13,data)
 
 def main():
-   set_led(led1,bankA)
+   set_led(3, bankA)
 
 if __name__ == "__main__": main()

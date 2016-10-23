94b4e46e8787481678ad0d144bd9d249b03735ec
CallumKendrick <callumkendrick@hotmail.co.uk>
2016-10-23 11:06:44 +0100

    maybe I fixed the thing I broke

--- node/html/script.js
+++ node/html/script.js
@@ -78,6 +78,8 @@ function drawCurrentlyAnsweringQuestion() {
 
 function nextQuestion() {
     var next = $("#questions")[0].children[0];
+    $("#current-question").html(next);
+    $("#current-question").html();
 }
 
 function sendQuestion() {
